fd1e2aa4ebaf33f0a6da676c9cb0160f583f0818
Mr.Ülg3n <68772806+omertugrulbayram@users.noreply.github.com>
2022-10-18 20:45:16 +0300

    Create Setupper.py

---
+++ Setupper.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import os
+
+os.system("clear")
+os.system("termux-setup-storage")
+os.system("pkg install git")
+os.system("pkg install curl")
+os.system("pkg install wget")
+os.system("pkg install python")
+os.system("pkg install python2")
+os.system("pkg install python3")
+os.system("pkg install php")
+os.system("pkg install perl")
+os.system("pkg install nano")
+os.system("pkg install vim")
+os.system("pkg install cat")
+os.system("pkg install pip")
+os.system("pkg install pip2")
+os.system("pkg install wordlist")
+os.system("pkg install nmap")
+os.system("pkg install hydra")
+os.system("pkg install openssl")
+os.system("pkg install nodejs")
+os.system("pkg install cowsay")
+os.system("clear")
+os.system("cowsay FİNİSH")
